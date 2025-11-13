be1404dbe24384f271ab260549f1c1cdeec1c8bc
D-Colak <denniscolak08@gmail.com>
2025-11-13 01:35:14 -0500

    added color sensing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -3,14 +3,23 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import static org.firstinspires.ftc.teamcode.HardwareUtility.CRServoInit;
 import static org.firstinspires.ftc.teamcode.HardwareUtility.motorInit;
 
+import android.graphics.Color;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorImpl;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 import org.firstinspires.ftc.teamcode.Settings;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Intake implements Subsystem {
     private LinearOpMode opMode;
@@ -19,10 +28,17 @@ public class Intake implements Subsystem {
     public NormalizedColorSensor colorSensor;
     public DistanceSensor distanceSensor;
 
+    public boolean usingColorSensor = false;
+
     NormalizedRGBA colors;
     public float[] hsvValues = {0F, 0F, 0F};
 
-    public DcMotor intake;
+    private static final float GREEN_HUE  = 120f;
+    private static final float PURPLE_HUE = 285f;
+    private static final float HUE_TOLERANCE = 25f; // +- degrees
+    private static final float ALPHA_THRESHOLD = 0.05f; // minimum alpha (0–1)
+
+    public boolean detectingBall = false;
 
     public Intake(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -32,8 +48,26 @@ public class Intake implements Subsystem {
     public void init() {
         intake = motorInit(opMode.hardwareMap, "intake", DcMotorSimple.Direction.REVERSE);
         intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        colorSensor = opMode.hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
-        distanceSensor = opMode.hardwareMap.get(DistanceSensor.class, "colorSensor");
+        if (usingColorSensor) {
+            colorSensor = opMode.hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+            distanceSensor = opMode.hardwareMap.get(DistanceSensor.class, "colorSensor");
+        }
+    }
+
+
+    @Override
+    public void update() {
+        if (!usingColorSensor) {
+            detectingBall = false;
+            return;
+        }
+
+        colors = colorSensor.getNormalizedColors();
+        Color.colorToHSV(colors.toColor(), hsvValues);
+        detectingBall = colors.alpha > ALPHA_THRESHOLD && (
+                hueDistance(hsvValues[0], GREEN_HUE) < HUE_TOLERANCE ||
+                hueDistance(hsvValues[0], PURPLE_HUE) < HUE_TOLERANCE
+        );
     }
 
     public void intake() {
@@ -59,10 +93,27 @@ public class Intake implements Subsystem {
 
     @Override
     public String[] getTelemetry() {
-        return new String[] {
-                "Intake Power: " + intake.getPower(),
-                "R: " + colors.red + " G: " + colors.green + " B: " + colors.blue + " A: " + colors.alpha,
-                "Ball Detected: " + (detectingBall ? "yes" : "no")
+        String[] colorArr;
+        if (usingColorSensor) {
+            colorArr = new String[]{
+                    "R: " + colors.red + " G: " + colors.green + " B: " + colors.blue + " A: " + colors.alpha,
+                    "Ball Detected: " + (detectingBall ? "yes" : "no")
+            };
+        } else {
+            colorArr = new String[0];
+        }
+
+        String[] normalArr = new String[] {
+            "Intake Power: " + intake.getPower(),
         };
+
+        return Stream.concat(Arrays.stream(normalArr), Arrays.stream(colorArr))
+                .toArray(String[]::new);
+    }
+
+
+    private static float hueDistance(float h1, float h2) {
+        float distance = Math.abs(h1 - h2);
+        return (distance > 180) ? 360 - distance : distance;
     }
 }
